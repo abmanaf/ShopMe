4a47b2ce81219571b0a23d31ddb26c88c1cac175
Alibaba <abdulmanafaliu414@gmail.com>
2024-01-08 00:04:14 +0000

    Changes made

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import './About.css'
 
 function About() {
+  const [showMore, setShowMore] = useState(false)
+
+  const handleShowMore = () => {
+    setShowMore(!showMore);
+  }
+
   return (
     <div className="about-container">
-      <div className="waiting-emoji">
-        <img src="" alt="" />
+      <div className="about-content">
+        <h2 className="about-heading">About Us</h2>
+        <p className="about-text">
+          {showMore ? (
+            <>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </>
+          ) : (
+            <>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting...
+            </>
+          )}
+        </p>
+  
+        <button className="submit-message" type="button" onClick={handleShowMore}>
+          {showMore ? "Show Less" : "Show More"}
+        </button>
       </div>
-      <p className="about-text" style={{ textAlign: "center" }}>About Coming Soon</p>
+      <div className="about-image"></div>
     </div>
+  
   );
 }
 
