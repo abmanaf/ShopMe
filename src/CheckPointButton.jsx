4951911b6950b3896692b5e789e0820c64de3a2c
Alibaba <abdulmanafaliu414@gmail.com>
2024-01-14 17:54:57 +0000

    a lot changes made

--- src/CheckPointButton.jsx
+++ src/CheckPointButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
-import "./CheckPointButton.css"; // Import the CSS file
+import { useNavigate } from "react-router-dom"; 
+import "./CheckPointButton.css"; 
 
 const CheckPointButton = () => {
   const location = useLocation();
