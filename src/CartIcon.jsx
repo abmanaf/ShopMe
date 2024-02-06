9551dcf531f98828bf7a4dd89b5ff749060fd9fe
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-06 07:30:17 +0000

    change made

--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const CartIcon = ({ cartCount }) => {
   return (
     <div>
-      <Link to="/CartContent" className="cart-icon" style={{ marginRight: "" }}>
+      <Link to="/E-commerce-react/CartContent" className="cart-icon" style={{ marginRight: "" }}>
         <i className="fa fa-shopping-cart"></i>
         <span className="cart-badge">{cartCount}</span>
       </Link>
