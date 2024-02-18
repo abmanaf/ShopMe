3c40bd71bd68d436b0e415e00b706d7809ab3c1a
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-18 12:07:09 +0000

    changes made

---
+++ WhatsApp_icon/WhatsAppIcon.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./whatsAppIcon.css";
+
+export const WhatsAppIcon = () => {
+  const whatsappNumber = "+233550800034";
+  const whatsappMessage = "Hello, I have a question about your website.";
+
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(
+    whatsappMessage
+  )}`;
+
+  return (
+    <div className="chat-me">
+      <a
+        className="whatsup-docaration"
+        href={whatsappLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="inside-whatsup-icon">Need Help? Chat Us</span>
+        <i
+          className="fa fa-whatsapp"
+          aria-hidden="true"
+          style={{ color: "green", fontSize: "2rem" }}
+        ></i>
+      </a>
+    </div>
+  );
+};
