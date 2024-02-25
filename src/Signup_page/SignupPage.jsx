15bc3a12548eb8cdf35a8166d06fe4257f4bb741
Alibaba <abdulmanafaliu414@gmail.com>
2024-02-25 05:55:20 +0000

    changes made

--- src/Signup_page/SignupPage.jsx
+++ src/Signup_page/SignupPage.jsx
@@ -114,6 +114,33 @@ function SignupPage({ updateEnteredDetails }) {
           </p>
         </form>
       </div>
+      {/* 
+      <div>
+      <h2 style={{ textAlign: "center" }}> User Details</h2>
+      <table className="product-table" style={{ margin: "0 auto" }}>
+        <thead>
+          <tr style={{ textAlign: "center" }}>
+            <th>First-name</th>
+            <th>Last-name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {enteredDetails.map((user) => (
+            <tr key={user.id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+              <td>{user.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    */}
     </div>
   );
 }
